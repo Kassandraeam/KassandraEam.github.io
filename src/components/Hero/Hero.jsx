56d3cecc68243c8b61b6f97866d55314ce947825
Kas Eam <KassandraEam@gmail.com>
2022-09-23 21:12:17 -0500

    Added some text and a typewriter effect.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import {
   PaperAirplaneIcon,
   ServerIcon,
 } from '@heroicons/react/solid'
-import bgImg from '../../assets/cyber-bg-1.png';
-
+import bgImg from '../../assets/futuristic-technology.gif';
+import TypeWriterEffect from 'react-typewriter-effect'
 
 const Hero = () => {
   return (
@@ -15,12 +15,20 @@ const Hero = () => {
     <div className='w-full h-screen bg-zinc-280 flex flex-col justify-between'>
       <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
         <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
-          <p className='text-2xl'>Unique Sequencing & Productions</p>
-          <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud management</h1>
-          <p className='text-2xl'>This is our tech brand.</p>
+          <p className='text-2xl justify mt-5'>Full Stack Software Engineer</p>
+          <div className = 'py-3 text-5xl md:text-7xl font-bold'>
+              <TypeWriterEffect
+              startDelay={10}
+              cursorColor="black"
+              text="Hi, I'm Kas!"
+              typeSpeed={100}
+              eraseSpeed={100}/>
+          </div>
+          <p className='text-1xl'>I'm a Full Stack Software engineer and future Kendama legend based in Minneapolis.</p>
+          <p className='text-1xl'>I have a passion for science, tech, diversity and inclusion, and problem solving.</p>
           <button className='py-3 px-6 sm:w-[60%] my-4'>Get started</button>
         </div>
-        <div>
+        <div className='w-full'>
           <img src={bgImg} alt="/"/>
         </div>
       </div>
